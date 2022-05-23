de6110acb7d57997050889134a45b3d7377fbe46
Emanue Angelo <emanuelangelo1@hotmail.com>
2022-05-23 15:16:27 -0300

    primeiro commit

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function verifica() {
+console.log("foi")
+}
+
+
+
+
+
+
+
+
+
+var table = document.getElementById("mytab1");
+for (var i = 0, row; row = table.rows[i]; i++) {
+   //iterate through rows
+   //rows would be accessed using the "row" variable assigned in the for loop
+   for (var j = 0, col; col = row.cells[j]; j++) {
+     //iterate through columns
+     //columns would be accessed using the "col" variable assigned in the for loop
+   }  
+}
